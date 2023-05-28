fa85d7b319bc030b56ad176f1949085955467cad
AstonDV <astondv.kms@gmail.com>
2023-05-28 21:54:13 +1000

    Create class Main

--- src/Notebook.java
+++ src/Notebook.java
@@ -1,22 +1,28 @@
 public class Notebook {
     private String brand;
+    private String model;
     private int ramSize;
     private int hddSize;
-    private String operatingSystem;
+    private String os;
     private String color;
     private double price;
 
-    public Notebook(String brand, int ramSize, int hddSize, String operatingSystem, String color, double price) {
+    public Notebook(String brand, String model, int ramSize, int hddSize, String os, String color, double price) {
         this.brand = brand;
+        this.model = model;
         this.ramSize = ramSize;
         this.hddSize = hddSize;
-        this.operatingSystem = operatingSystem;
+        this.os = os;
         this.color = color;
         this.price = price;
     }
 
     public String getBrand() {
         return brand;
+    }
+
+    public String getModel() {
+        return model;
     }
 
     public int getRamSize() {
@@ -27,8 +33,8 @@ public class Notebook {
         return hddSize;
     }
 
-    public String getOperatingSystem() {
-        return operatingSystem;
+    public String getOs() {
+        return os;
     }
 
     public String getColor() {
